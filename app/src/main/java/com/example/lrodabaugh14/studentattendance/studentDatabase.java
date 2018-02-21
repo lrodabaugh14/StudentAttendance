61cc7fc74f8157e10cce944d926a11b32941fae5
jkrajicek <jill@igroupcreative.com>
2018-02-21 13:31:10 -0600

    Cleaned up the database class to resuse repeated code and removed unneccesary threading

--- app/src/main/java/com/example/lrodabaugh14/studentattendance/studentDatabase.java
+++ app/src/main/java/com/example/lrodabaugh14/studentattendance/studentDatabase.java
@@ -39,51 +39,43 @@ public class studentDatabase {
     }
 
 
-    public boolean LoginAttempt(String username, String passwords) throws InterruptedException {
-        username = "candace_farmer";
-        final String password = "password";
-        DatabaseReference dbFaculty = dbRef.child("Faculty");
+    public boolean LoginAttempt(String username, String password) throws InterruptedException {
+
+        DatabaseReference user = dbRef.child("Faculty").child(username);
         DatabaseReference dbStudent = dbRef.child("Students");
         DatabaseReference refClasses = dbRef.child("Classes");
-        DatabaseReference user = dbFaculty.child(username);
 
-
-        final List<Object> userInfo = getUserInfo(user);
-        List<Object> students = getStudents(dbStudent);
-        final List<Object> classes = getClassInfo(refClasses);
+        final List<Object> userInfo = getDbInfo(user);
+        List<Object> students = getDbInfo(dbStudent);
+        final List<Object> classes = getDbInfo(refClasses);
 
         boolean matching = false;
         Thread.sleep(1000);
         if (!userInfo.isEmpty()) {
             HashMap<String, Object> hashMap = (HashMap<String, Object>) userInfo.get(0);
             String strPassword = (String) hashMap.get("Password");
-            AppUtil.strFacID = (String) hashMap.get("ID");
-            facNumber = (String) hashMap.get("ID");
+            AppUtil.strFacID = String.valueOf(hashMap.get("ID"));
+            facNumber = String.valueOf(hashMap.get("ID"));
             matching = strPassword.equals(password);
         }
-        if (!students.isEmpty()) {
-            // AppUtil.hshStudents = (HashMap<String, Object>) students.get(0);
-            AppUtil.arrStudents = (ArrayList<Object>) students.get(0);
+        if(matching){
+            if (!students.isEmpty()) {
+                AppUtil.arrStudents = (ArrayList<Object>) students.get(0);
+            }
+            if (!classes.isEmpty()) {
+                AppUtil.hashClasses = (HashMap<String, Object>) classes.get(0);
+            }
         }
-        Thread t1 = new Thread(new Runnable() {
-            public void run()
-            {
-                if (!classes.isEmpty()) {
-                    AppUtil.hashClasses = (HashMap<String, Object>) classes.get(0);
-                }
-            }});
-//        Thread.sleep(4000);
-        t1.start();
-        Thread.sleep(1000);
+
         return matching;
     }
-    public List<Object> getStudents(DatabaseReference student) {
-        final List<Object> studentInfo = new ArrayList();
-        student.addValueEventListener(
+    public List<Object> getDbInfo(DatabaseReference dbRef) {
+        final List<Object> info = new ArrayList();
+        dbRef.addValueEventListener(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        studentInfo.add(dataSnapshot.getValue());
+                        info.add(dataSnapshot.getValue());
                         return;
                     }
 
@@ -94,55 +86,7 @@ public class studentDatabase {
                 }
 
         );
-        return studentInfo;
-    }
-//
-//    public List<Object> getClassesByTeacher() throws InterruptedException {
-//
-//
-//        return classes;
-//
-//
-//
-//    }
-
-    public List<Object> getClassInfo(DatabaseReference refClasses) {
-        final List<Object> classes = new ArrayList();
-        refClasses.orderByValue()
-                .addListenerForSingleValueEvent(
-                        new ValueEventListener() {
-                            @Override
-                            public void onDataChange (DataSnapshot dataSnapshot) {
-                                classes.add(dataSnapshot.getValue());
-                            }
-                            @Override
-                            public void onCancelled (DatabaseError databaseError){
-
-                            }
-                        }
-
-                );
-        return classes;
-    }
-
-    public List<Object> getUserInfo(DatabaseReference user) {
-        final List<Object> userInfo = new ArrayList();
-        user.addValueEventListener(
-                new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            userInfo.add(dataSnapshot.getValue());
-            return;
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError){
-
-        }
-    }
-
-                );
-        return userInfo;
+        return info;
     }
 
     public void submitAttendance(String strClass, List<String> absentStudents){
